7abf2cfda125373b7511f86a967cba80b9d4a8cb
Jake Cleary <jake.cleary@mediatonic.co.uk>
2020-12-12 01:27:22 +0000

    Complete 02:02

--- aoc_2020_02/src/main.rs
+++ aoc_2020_02/src/main.rs
@@ -8,11 +8,16 @@ fn main() {
     let buffer = BufReader::new(file);
     let lines: HashSet<String> = buffer.lines().map(|l| l.unwrap()).collect();
     let regex = Regex::new(r"^([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)$").unwrap();
-    let valid_lines = lines.iter().filter(|line| regex.is_match(line));
-    let valid_passwords = valid_lines
+    let parsed_lines: HashSet<(char, usize, usize, &str)> = lines.iter()
+        .filter(|line| regex.is_match(line))
         .map(|line| unwrap_captures(regex.captures(line).unwrap()))
-        .filter(|config| is_valid_password(*config));
-    println!("Count: {}", valid_passwords.count());
+        .collect();
+    let valid_sled_shop_passwords = parsed_lines
+        .iter().filter(|config| is_valid_sled_rental_shop_password(config));
+    let valid_nptrs_passwords = parsed_lines
+        .iter().filter(|config| is_valid_nptrs_password(config));
+    println!("Valid Sled Shop passwords: {}", valid_sled_shop_passwords.count());
+    println!("Valid NPTRS passwords: {}", valid_nptrs_passwords.count());
 }
 
 fn unwrap_captures(capture: regex::Captures) -> (char, usize, usize, &str) {
@@ -23,8 +28,17 @@ fn unwrap_captures(capture: regex::Captures) -> (char, usize, usize, &str) {
     (letter, min_count, max_count, password)
 }
 
-fn is_valid_password(config: (char, usize, usize, &str)) -> bool {
-    let (letter, min_count, max_count, password) = config;
+fn is_valid_sled_rental_shop_password(config: &(char, usize, usize, &str)) -> bool {
+    let (letter, min_count, max_count, password) = *config;
     let count = password.chars().filter(|character| character == &letter).count();
     count >= min_count && count <= max_count
+}
+
+fn is_valid_nptrs_password(config: &(char, usize, usize, &str)) -> bool {
+    let (letter, position_1, position_2, password) = *config;
+    let position_1_valid = password.chars().nth(position_1 - 1) == Some(letter);
+    let position_2_valid = password.chars().nth(position_2 - 1) == Some(letter);
+    let neither_valid = !position_1_valid && !position_2_valid;
+    let both_valid = position_1_valid && position_2_valid;
+    (!neither_valid && !both_valid) && (position_1_valid || position_2_valid)
 }
